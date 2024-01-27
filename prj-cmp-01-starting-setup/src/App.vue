8aa529d9a35d85cbc1757272cdd7872e44c8eb02
Antti Helander <antti.helander@hubble.fi>
2024-01-27 17:03:51 +0200

    Added prj-cmp-01... folder

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <list-of-web-sites
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.tittle"
+      :description="resource.description"
+      :link="resource.link"
+    ></list-of-web-sites>
+  </ul>
+</template>
+<script>
+import ListOfWebSites from '@/components/learning-resources/ListOfWebSites.vue';
+export default {
+  components: {
+    ListOfWebSites,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          tittle: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tittle: 'Google',
+          description: 'Learn to Google!',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
