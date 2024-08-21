dc580673653b98be2d1766302247d95ad49fd106
Antti Helander <hela_ant@outlook.com>
2024-08-21 16:09:09 +0300

    Added Get method and erorr handling.

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -21,6 +21,7 @@
           <label for="rating-great">Great</label>
         </div>
         <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -36,6 +37,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null
     };
   },
   emits: ['survey-submit'],
@@ -51,7 +53,7 @@ export default {
       //   userName: this.enteredName,
       //   rating: this.chosenRating,
       // });
-
+      this.error = null;
       fetch("https://vue-http-demo-405fd-default-rtdb.europe-west1.firebasedatabase.app/surveys.json", {
         method: "POST",
         headers: {
@@ -60,7 +62,14 @@ export default {
         body: JSON.stringify({
           name: this.enteredName,
           rating: this.chosenRating
-        })
+        }),
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error("Could not save data!")
+        }
+      }).catch(error => {
+        console.log(error);
+        this.error = error.message;
       });
 
       this.enteredName = '';
--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,15 +3,16 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+      <p v-if="isLoading">Loading data...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored Experiences found. Start by adding
+        one.</p>
+
+      <ul v-else>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
       </ul>
     </base-card>
   </section>
@@ -21,10 +22,44 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null
+    }
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch("https://vue-http-demo-405fd-default-rtdb.europe-west1.firebasedatabase.app/surveys.json").then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+      }
+      ).then((data) => {
+        this.isLoading = false;
+        const tempResults = [];
+        for (const id in data) {
+          tempResults.push({ id: id, name: data[id].name, rating: data[id].rating });
+        }
+        this.results = tempResults;
+
+      }).catch((error) => {
+        this.isLoading = false
+        this.error = error + "Failed to fetch data. Please try again later"
+      });
+
+    },
+
+  },
+  mounted() {
+    this.loadExperiences();
+  }
 };
 </script>
 
